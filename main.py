6d2550afedb2d5c922f52e366de86202c05d9c86
Furior <furiorg@gmail.com>
2025-06-10 03:12:20 +0700

    somecode

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import random
 
-def generate_random_number():
-    """Generate a random number between 1 and 100"""
-    return random.randint(1, 100)
+def generate_random_number(min_value=1, max_value=100):
+    """Generate a random number between min_value and max_value"""
+    return random.randint(min_value, max_value)
 
 def main():
     random_number = generate_random_number()
     print(f"Random number: {random_number}")
-    if random_number > 50:
-        print("The number is greater than 50!")
+    if random_number > 75:
+        print("The number is greater than 75!")
     else:
-        print("The number is less than or equal to 50!")
+        print("The number is less than or equal to 75!")
+        # Oops, I forgot to close this file!
+        file = open("output.txt", "w")
+        file.write(f"Random number: {random_number}")
 
 if __name__ == "__main__":
     main()
